c74a54b00925f4dc25d06ea3f68aa79a7767921d
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-05-04 09:09:34 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,12 +5,26 @@ let scrolling = false;
 const THROTTLE_DELAY = 500;
 let lastAnimationTime = 0;
 
-const canvas = document.getElementById('header_background');
-const ctx = canvas.getContext('2d');
+const canvasID = [];
+
+sections.forEach(element => {
+  canvasID.push(element.id + "_background");
+});
+
+const canvasColor = [
+  ["black", "lightgray", "black"],
+  ["lightgray", "black", "black"],
+  ["white", "black", "black"],
+  ["white", "black", "black"],
+];
+
+let canvas = document.getElementById(canvasID[currentSection]);
+let ctx = canvas.getContext('2d');
 
 const cellSize = 50; // Taille d'une cellule
 let rows, cols;
 let hoverCell = { row: -1, col: -1 };
+
 
 window.addEventListener('wheel', (event) => {
   const now = Date.now();
@@ -35,11 +49,14 @@ window.addEventListener('wheel', (event) => {
 
 // scrollToSection accepte un second paramètre "isWheel" pour activer le blocage temporaire
 function scrollToSection(index, isWheel = false) {
-  console.log("Scroll vers section", index);
   if (isWheel) scrolling = true;
 
   currentSection = index;
+  setCanvas();
+  resizeCanvas();
   sections[index].scrollIntoView({ behavior: 'smooth' });
+  
+  changeColorButton();
 
   if (isWheel) {
     setTimeout(() => {
@@ -56,12 +73,27 @@ function initNavButtons() {
 
     button.addEventListener("click", () => {
       const index = Array.from(sections).indexOf(element);
+      
       scrollToSection(index); // appel sans "isWheel" → pas de blocage
     });
 
     button.classList.add("button");
     container.appendChild(button);
   });
+}
+
+function changeColorButton() {
+  const container = document.getElementById("button-container");
+  const childsList = container.children;
+
+  for (let i = 0; i < childsList.length; i++) {
+    if (i == currentSection) {
+      childsList[i].style.backgroundColor = "white";
+    }
+    else {
+      childsList[i].style.backgroundColor = "black";
+    }
+  }
 }
 
 initNavButtons();
@@ -82,13 +114,13 @@ function drawGrid() {
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       if (isHoveredOrNeighbor(row, col)) {
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = canvasColor[currentSection][0];
       } else {
-        ctx.fillStyle = 'lightgray';
+        ctx.fillStyle = canvasColor[currentSection][1];
       }
 
       ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
-      ctx.strokeStyle = 'black';
+      ctx.strokeStyle = canvasColor[currentSection][2];
       ctx.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
     }
   }
@@ -98,28 +130,37 @@ function isHoveredOrNeighbor(row, col) {
   const r = hoverCell.row;
   const c = hoverCell.col;
 
-  return (
-    (row === r && col === c) || // cell hovered
-    (row === r - 1 && col === c) || // haut
-    (row === r + 1 && col === c) || // bas
-    (row === r && col === c - 1) || // gauche
-    (row === r && col === c + 1)    // droite
-  );
+  if (currentSection == 0) {
+    return (
+      (row === r && col === c) || // cell hovered
+      (row === r - 1 && col === c) || // haut
+      (row === r + 1 && col === c) || // bas
+      (row === r && col === c - 1) || // gauche
+      (row === r && col === c + 1)    // droite
+    );
+  }
+  else {
+    return (row === r && col === c);
+  }
 }
 
-canvas.addEventListener('mousemove', (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+function setCanvas() {
+  canvas = document.getElementById(canvasID[currentSection]);
+  ctx = canvas.getContext('2d');
+  canvas.addEventListener('mousemove', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-  const col = Math.floor(x / cellSize);
-  const row = Math.floor(y / cellSize);
+    const col = Math.floor(x / cellSize);
+    const row = Math.floor(y / cellSize);
 
-  if (row !== hoverCell.row || col !== hoverCell.col) {
-    hoverCell = { row, col };
-    drawGrid();
-  }
-});
+    if (row !== hoverCell.row || col !== hoverCell.col) {
+      hoverCell = { row, col };
+      drawGrid();
+    }
+  });
+}
 
 window.addEventListener('resize', resizeCanvas);
 
